7c923632a196fe4d0fe3f004fa999cb50cdd8468
zchuwie <mayaloveptr.0108@gmail.com>
2024-03-21 02:58:54 +0000

    commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM windows:latest
+
+RUN apt-get update && apt-get install -y \
+    python3.10 \
+    python3-pip \
+    git add 
+
+RUN pip3 install PyYAML
+
+COPY feed.py /urs/bin/feed.py
+
+COPY entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "============="
+
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${INPUT_EMAIL}"
+git config --global -add safe.directory /github/workspace
+
+python3 /urs/bin/feed.py
+
+git add -A && git commit -m "Update Feed"
+
+fir push --set-upstream origin main
+
+
+echo "============="
